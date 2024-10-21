9bbca5c8e99190fb4051d5862d249ae0843b0f50
tnguy510 <tnguy510@ucsc.edu>
2024-10-21 16:24:55 -0700

    Step 1 Complete

--- src/main.ts
+++ src/main.ts
@@ -1,7 +1,19 @@
 import "./style.css";
 
-const APP_NAME = "Hello";
+const APP_NAME = "Blows You Up";
 const app = document.querySelector<HTMLDivElement>("#app")!;
+const header = document.createElement("h1");
 
 document.title = APP_NAME;
 app.innerHTML = APP_NAME;
+header.innerHTML = "Drawing Time";
+app.append(header);
+
+const canvas = document.createElement("canvas");
+canvas.width = canvas.height = 256;
+
+//const ctx = <CanvasRenderingContext2D>canvas.getContext("2d");
+app.append(canvas);
+
+//ctx.fillStyle = "green";
+//ctx.fillRect(10, 10, 150, 100);
