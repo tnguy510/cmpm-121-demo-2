3da66de14b921410414c55d7a5fd65d30a81cfee
tnguy510 <tnguy510@ucsc.edu>
2024-10-21 16:44:58 -0700

    Step 2 Complete

--- src/main.ts
+++ src/main.ts
@@ -12,8 +12,51 @@ app.append(header);
 const canvas = document.createElement("canvas");
 canvas.width = canvas.height = 256;
 
-//const ctx = <CanvasRenderingContext2D>canvas.getContext("2d");
+const ctx = <CanvasRenderingContext2D>canvas.getContext("2d");
 app.append(canvas);
 
-//ctx.fillStyle = "green";
-//ctx.fillRect(10, 10, 150, 100);
+ctx.strokeStyle = "black";
+ctx.lineWidth = 2;
+
+let cursorX, cursorY = 0;
+let isDrawing = false;
+
+
+canvas.addEventListener("mousedown", (event) => {
+    cursorX = event.offsetX;
+    cursorY = event.offsetY;
+    isDrawing = true;
+});
+
+canvas.addEventListener("mousemove", (event) => {
+    if(isDrawing) {
+        drawLine(ctx, cursorX, cursorY, event.offsetX, event.offsetY);
+        cursorX = event.offsetX;
+        cursorY = event.offsetY;
+    }
+});
+
+canvas.addEventListener("mouseup", (event) => {
+    if(isDrawing) {
+        drawLine(ctx, cursorX, cursorY, event.offsetX, event.offsetY);
+        cursorX = 0;
+        cursorY = 0;
+        isDrawing = false;
+    }
+});
+
+function drawLine(line: CanvasRenderingContext2D, x1: number, y1: number, x2: number, y2: number) {
+    line.beginPath();
+    line.moveTo(x1, y1);
+    line.lineTo(x2, y2);
+    line.stroke();
+    line.closePath();
+}
+
+const clearButton = document.createElement("button");
+clearButton.innerHTML = "Clear Canvas";
+app.append(clearButton);
+
+clearButton.addEventListener("click", () => {
+    ctx.clearRect(0, 0, canvas.width, canvas.height);
+})
